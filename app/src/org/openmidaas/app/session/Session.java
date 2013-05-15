ec88f027c079c882fc3d4e83b086324834a6b136
adas2k <adas2k@gmail.com>
2013-05-15 11:17:49 -0400

    small fix

--- app/src/org/openmidaas/app/session/Session.java
+++ app/src/org/openmidaas/app/session/Session.java
@@ -293,7 +293,8 @@ public class Session implements VerifiedAttributeBundleCallback{
 	}
 	
 	/**
-	 * Authorizes the release of attributes to the RP for this session. 
+	 * Authorizes the release of attributes to the RP for this session. First tries to get the verified bundle, then the 
+	 * unverified bundle and then returns the data to the RP. 
 	 * @param onDoneCallback the callback handler to get the status of the authorization
 	 * @throws EssentialAttributeMissingException if no attribute is provided for a required attribute. 
 	 */
