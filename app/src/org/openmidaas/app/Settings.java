13480fb827489432152c649773487e5fe2a51d49
Shobhit Puri <shobhit.iiita@gmail.com>
2013-07-15 12:01:50 -0400

    settings file spacing

--- app/src/org/openmidaas/app/Settings.java
+++ app/src/org/openmidaas/app/Settings.java
@@ -18,12 +18,12 @@ package org.openmidaas.app;
 import org.openmidaas.library.MIDaaS;
 
 public final class Settings {
-public static final boolean SHOULD_LOG = true;
-public static final boolean ATTRIBUTE_DIAGNOSTICS_ENABLED = true;
-public static final int LIBRARY_LOG_LEVEL = MIDaaS.LOG_LEVEL_DEBUG;
-public static final boolean IS_HOCKEY_APP_ENABLED = false;
-public static final String HOCKEY_APP_ID = "";
-public static final String SERVER_URL = "https://midaas-avp.securekeylabs.com";
-public static final String GCM_SENDER_ID = "";
-public static final String PUSH_REGISTRATION_SERVER_URL = "";
+	public static final boolean SHOULD_LOG = true;
+	public static final boolean ATTRIBUTE_DIAGNOSTICS_ENABLED = true;
+	public static final int LIBRARY_LOG_LEVEL = MIDaaS.LOG_LEVEL_DEBUG;
+	public static final boolean IS_HOCKEY_APP_ENABLED = false;
+	public static final String HOCKEY_APP_ID = "";
+	public static final String SERVER_URL = "https://midaas-avp.securekeylabs.com";
+	public static final String GCM_SENDER_ID = "";
+	public static final String PUSH_REGISTRATION_SERVER_URL = "";
 }
